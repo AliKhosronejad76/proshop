1e66461b5e6febe80b738a59b00ae532f36ea3df
Ali Khosronejad <khosronejadali344@gmail.com>
2024-07-03 22:17:20 +0330

    commit

--- src/context/ContextProvider.jsx
+++ src/context/ContextProvider.jsx
@@ -2,12 +2,14 @@ import UserModalProvider from "./UserModalProvider"
 import DrawerContextProvider from "./DrawerContextProvider"
 export default function ContextProvider({children}){
     return(
-        <>
+       <>
             <UserModalProvider>
                 <DrawerContextProvider>
                    {children}
                 </DrawerContextProvider>
             </UserModalProvider>
-        </>
+       </>
+           
+        
     )
 }
