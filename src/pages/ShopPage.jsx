e2ce5d505f5504bf1f90c9de70cd953ef95a814c
Ali Khosronejad <khosronejadali344@gmail.com>
2024-07-04 21:23:03 +0330

    aside and  main created

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,7 +1,12 @@
+import Aside from "../components/shoppage/aside/Aside"
+import Main from "../components/shoppage/main/Main"
+
+
 export default function ShopPage(){
       return(
             <div className="bg-[#f0f0f1] pt-4 px-7 flex">
-                  
+                <Aside/>
+                <Main/>  
             </div>
       )
 }
