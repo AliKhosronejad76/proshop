ee4ac325d2f94ee9342d36ce4f4e5effcc54e631
Ali Khosronejad <khosronejadali344@gmail.com>
2024-07-04 21:16:42 +0330

    shopPage add to routr.jsx

--- src/components/router/Router.jsx
+++ src/components/router/Router.jsx
@@ -1,10 +1,12 @@
 import { Route , Routes } from "react-router-dom"
 import HomePage from "../../pages/HomePage"
+import ShopPage from "../../pages/ShopPage"
 
 export default function Router(){
     return(
         <Routes>
-            <Route index path="/" element={<HomePage/>}/>
+            <Route index path="/" element={<HomePage/>} />
+            <Route path="/shop"  element={<ShopPage/>}  />
         </Routes>
     )
 }
