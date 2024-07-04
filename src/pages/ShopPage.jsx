0bff6cb9fa6854a70d1fd6dbda964e71e67bf976
Ali Khosronejad <khosronejadali344@gmail.com>
2024-07-04 21:20:49 +0330

    main.jsx and aside.jsx created

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,7 +1,7 @@
 export default function ShopPage(){
       return(
             <div className="bg-[#f0f0f1] pt-4 px-7 flex">
-                  this is a shoping page
+                  
             </div>
       )
 }
